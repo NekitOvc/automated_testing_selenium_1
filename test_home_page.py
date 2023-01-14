b5cd86d04faad50c179e7ba5e7fe4954705cc8cc
Nikita Ovchinnikov <108588900+NekitOvc@users.noreply.github.com>
2023-01-15 00:31:49 +0300

    Update test_home_page.py

--- test_home_page.py
+++ test_home_page.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options as chrome_options
 
 import pytest
